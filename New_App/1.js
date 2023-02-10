0c315ca797aa5f275d85cd423235c1d1c42222a0
lavanyal <lavanyalakshita287@gmail.com>
2023-02-10 18:11:59 +0530

    updated

--- New_App/1.js
+++ New_App/1.js
@@ -19,9 +19,10 @@ if (todos) {
 function addtodo(elem) {
     // To Add ToDo
     let todoColl = document.createElement("div");
-    console.log("todoColl", todoColl);
+    // console.log("todoColl", todoColl);
+    todoColl.classList.add("todocoll");
     let todotext = text.value;
-    console.log("todotext", todotext);
+    // console.log("todotext", todotext);
     if (elem) {
         todotext = elem.text;
     }
@@ -34,7 +35,7 @@ function addtodo(elem) {
     </div>   
     <div class="hr"></div>`
         todoCon.appendChild(todoColl);
-        console.log("todoColl", todoColl);
+        // console.log("todoColl", todoColl);
         updateLs()
     }
 
@@ -51,9 +52,9 @@ function addtodo(elem) {
     let check = todoColl.querySelector(".check-div");
     check.addEventListener('click', () => {
         check.classList.toggle("active-check")
-        console.log(todoColl.children)
-        console.log(todoColl.children[0])
-        console.log(todoColl.children[0].children[1])
+        // console.log(todoColl.children)
+        // console.log(todoColl.children[0])
+        // console.log(todoColl.children[0].children[1])
         todoColl.children[0].children[1].classList.add("complete")
         updateLs()
     })
@@ -72,3 +73,61 @@ function updateLs() {
     });
     localStorage.setItem("todos", JSON.stringify(arr));
 }
+
+// when we click on active,we can only see todo which are active(not completed)
+// and when click on completed, we can see todo which are completed
+
+// let info = document.querySelectorAll(".info p")
+// console.log(info);
+let info = document.querySelectorAll(".active-cont p")
+// console.log(info);
+
+let todoli = document.querySelectorAll(".todocoll")
+// console.log(todoli);
+info.forEach(element => {
+    // console.log(element, "element");
+    element.addEventListener("click", () => {
+        info.forEach(item => {
+            item.classList.remove("active");
+        });
+        element.classList.add("active")
+
+        if (element.innerText == "Active") {
+            todoli.forEach(ele => {
+                // console.log("ele", ele.children);
+                // console.log("ele2", ele.children[0]);
+                // console.log("ele3", ele.children[0].children[1]);
+                if (!ele.children[0].children[1].classList.contains("complete")) {
+                    ele.style.display = "block";
+                } else {
+                    ele.style.display = "none";
+                }
+            })
+        } else if (element.innerText == "Completed") {
+            todoli.forEach(ele => {
+                if (ele.children[0].children[1].classList.contains("complete")) {
+                    ele.style.display = "block";
+                } else {
+                    ele.style.display = "none";
+                }
+            })
+        } else {
+            todoli.forEach(ele => {
+                ele.style.display = "block";
+            })
+        }
+    })
+})
+
+// clear all completed todo when click on clear todo
+
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    todoli.forEach(elem => {
+        // console.log("elem", elem);
+        if (elem.children[0].children[1].classList.contains("complete")) {
+            elem.remove()
+            updateLs();
+        }
+    });
+})
--- script.js
+++ script.js
@@ -19,7 +19,7 @@ themeBtn.onclick = () => {
 }
 
 // CRUD FUNCTIONALITY
-let arr_ids = [];
+let arr_todo = [];
 
 function add_click() {
     document.getElementById("error").innerHTML = "";
@@ -27,47 +27,38 @@ function add_click() {
     if (id == "") {
         document.getElementById("error").innerHTML = "Please Enter Value";
     } else {
-        arr_ids.push(id);
+        arr_todo.push({ todo: id, selected: false });
         display_data();
     }
+    console.log(arr_todo, 'todo array')
 }
-
-function showAll_click() {
-    display_data();
-}
-
-{/* <div class="check-div" onclick="select_click()"><img src="./images/icon-check.svg"  /></div> */ }
+console.log(arr_todo, 'todo array')
 function display_data() {
     let div_Table = document.getElementById("list-grp");
-    let my_Table = `<div style="width:100%;">
+    let my_Table = `<div class="div_container" style="width:100%;" >
     `;
-    for (let i = 0; i < arr_ids.length; i++) {
+    // for (let i = 0; i < arr_todo.length; i++) {
+    arr_todo.map((todos, i) => (
         my_Table += `
-        <div style="display:flex; align-items:center;" class="list-group" >      
-            <div class="check-div"><img src="./images/icon-check.svg" /></div>
-            <p id="myData">${arr_ids[i]}</p>
-            <img src="./images/icon-cross.svg" class="cross" onclick="delete_click()"/>
-        </div>
-        <div class="hr"></div>
-        `;
-    }
+            <div  class="list-group" style="display:flex; align-items:center;" >      
+                <div class="check-div" onclick="select_click(${i})"><img src="./images/icon-check.svg" /></div>
+                <p id="myData" class="ptag">${todos.todo}</p>
+                <img src="./images/close.png" class="cross" onclick=delete_click(${i}) />
+            </div>
+            <div class="hr"></div>
+            `
+    ));
+    // }
     my_Table += `</div>`;
     div_Table.innerHTML = my_Table;
 }
 
-
-function select_click() {
-}
-
-function delete_click() {
-    let id = document.getElementById("myData").innerText;
-    i = arr_ids.indexOf(id);
-    if (i == -1) {
-        alert("id not found");
-    } else {
-        arr_ids.splice(i, 1);
-        display_data();
-    }
+// delete data on delete button click
+function delete_click(index) {
+    console.log(index, 'del click')
+    // console.log(id[index].innerHTML);
+    let id = document.querySelectorAll(".ptag");
+    let pid = id[index].innerHTML;
 }
 
 // add data on input enter
@@ -79,3 +70,46 @@ input.addEventListener("keypress", function (event) {
         input.value = "";
     }
 });
+
+// // add image when click on check button
+// // add active-check class on check button
+function select_click(ind) {
+    let id = document.querySelectorAll(".ptag");
+    id[ind].classList.add("complete");
+}
+
+// show all todo
+function showAll_click() {
+    display_data();
+}
+
+// show todo which are completed
+function completed_all() {
+    let id = document.querySelectorAll(".ptag");
+    for (let i = 0; i <= id.length; i++) {
+        if (id[i].classList.contains("complete")) {
+            id[i].style.display = "flex";
+        } else {
+            id[i].style.display = "none";
+        }
+    }
+}
+
+// show todo which are active
+function active_all() {
+    let id = document.querySelectorAll(".ptag");
+    for (let i = 0; i <= id.length; i++) {
+        if (!id[i].classList.contains("complete")) {
+            id[i].style.display = "flex";
+        } else {
+            id[i].style.display = "none";
+        }
+    }
+    // let id = document.getElementById("list-grp")
+    // console.log(id.children[0].children);
+}
+
+function completed_all() {
+
+}
+
